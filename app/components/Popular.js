b81c3c1ae7b460ac17dcb35d6a358b09f7e530b5
Matthew Eddy <meddy672@gmail.com>
2021-05-23 16:12:50 -0400

    added update language method to Popular.js component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,22 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            selectedLanguage: 'All'
+        };
+
+        this.updateSelectedLangugae = this.updateSelectedLangugae.bind(this)
+    }
+
+    updateSelectedLangugae(selectedLanguage) {
+        this.setState({
+            selectedLanguage: selectedLanguage
+        })
+    }
     
     render() {
         const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
@@ -8,7 +24,11 @@ export default class Popular extends React.Component {
             <ul className="flex-center">
                 {languages.map((language) => {
                     return (<li key={language}>
-                        <button className="btn-clear nav-link">
+                        <button
+                            className="btn-clear nav-link"
+                            style={language === this.state.selectedLanguage ? { color: 'rgb(187, 46, 31)' } : null}
+                            onClick ={() => this.updateSelectedLangugae(language)}
+                        >
                         { language}
                         </button>
                     </li>)
