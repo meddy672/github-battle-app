915bd3f142da9a029695f835c857a1428611573e
Matthew Eddy <meddy672@gmail.com>
2021-05-29 21:24:10 -0400

    added better documentation to api

--- app/api/Repositories.js
+++ app/api/Repositories.js
@@ -1,7 +1,6 @@
 /**
  * 
- * @param {string} language 
- * @returns 
+ * returns an array of popular repositories by language
  */
 export function fetchPopularRepos(language) {
     const endpoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
